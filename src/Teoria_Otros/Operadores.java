b6b6e50e848917d53f18a276a72aab7f476c3f9b
saraiolcina <sarai.olcina.g@gmail.com>
2020-10-08 18:46:58 +0200

    inicio operadores

--- src/Teoria_Otros/Operadores.java
+++ src/Teoria_Otros/Operadores.java
@@ -3,7 +3,45 @@ package Teoria_Otros;
 public class Operadores {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//TIPOS DE OPERADORES:
+		
+		/*ASIGNACIÓN*/
+		
+		int n=6;
+		int nn=-6;
+		
+		/*ARITMÉTICOS*/
+		
+		int suma= n+nn;
+		int resta= n-nn;
+		int producto=n*nn;
+		int division=n/nn;
+		int resto=n%nn;
+		
+		System.out.println(20%7);
+		
+		/*INCREMENTALES*/
+		
+		n++;
+		nn--;
+		--n;
+		++nn;		/*este operador también puede ir delante, y por lo tanto se aplicaría antes de la ejecución*/
+		
+		/*COMBINADOS: combinan un operador aritmético con el de asignación*/
+		n+=nn;		//n=n+nn
+		n-=nn;		//n=n-nn
+		n*=nn;		//n=n*nn
+		n/=nn;		//n=n/nn
+		n%=nn;		//n=n%nn
+		
+		/*DE RELACIÓN*/
+		/* n==nn;
+		 * n!=nn;
+		 * n>=nn;
+		 * n<=nn;
+		 * */
+		
+		
 
 	}
 
